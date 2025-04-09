a65ba450329c15259cd4369da953ae0713a74488
Pratham <pratham10600@gmail.com>
2025-04-09 23:43:57 +0530

    sum_prg

---
+++ sum_no.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "enter the 1st no : ";
+    cin >> a  ;
+    cout << "2nd no : ";
+    cin >> b;
+    cout << a+b;
+
+
+}
