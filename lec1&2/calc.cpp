c30355dbb2009743f92ab1b42202989625fc6e05
Pratham <pratham10600@gmail.com>
2025-04-10 00:50:46 +0530

    Structure maintained

---
+++ lec1&2/calc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "enter no 1: ";
+    cin >> a;
+    cout << "enter no 2 : ";
+    cin >>b;
+
+    cout << "sum : " << a+b << endl;
+    cout << "differnce : " <<a-b << endl;
+    cout << "multiply : " << a*b << endl;
+    cout << "divide : " <<a/b << endl;
+}
