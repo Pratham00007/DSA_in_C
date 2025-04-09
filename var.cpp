4fad489264c12151a304667d3a79312809498cbe
Pratham <pratham10600@gmail.com>
2025-04-09 22:58:27 +0530

    variables

---
+++ var.cpp
@@ -0,0 +1,12 @@
+// single cote for single aphabet and double cote for word
+#include <iostream>
+using namespace std;
+int main(){
+    int age =25;
+    cout << age << endl;
+    cout << sizeof(age);
+
+    // for float always write f or F at end else it eill be stored as double 
+
+// uses capslok for variable having constant value ex PI
+}
