610cb9671160c7fd46e62eff41143b47d31f8bcf
Pratham <pratham10600@gmail.com>
2025-04-09 22:38:20 +0530

    lec1

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// its preprossersor directive
+using namespace std;
+// to use cout we tell we are using std file's cout
+int main(){
+    // main means our programme start from here
+    cout << "Hello World";
+    // to tell you are using std file cout you can type this also
+    std::cout << "Hello world";
+    return 0;
+    // bcs this func work is to return a int value for good programmer
+}
---
+++ print2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello Pratham";
+    // endl = end of line now give output from next line
+    cout << "hello" << endl; 
+    cout << "world";
+// or use '\n' its better than endl
+    cout << "pussy" << '\n';
+    // or
+    cout << "cat\n";
+    
+    cout << "Pratham\nKhetrapal";
+
+    return 0;
+}
