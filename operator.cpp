c69a17a222a359e5b27858a25f66cd2b58ede6d2
Pratham <pratham10600@gmail.com>
2025-04-09 23:38:25 +0530

    operator

---
+++ operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    int a=5,b=10;
+    cout << a+b  <<endl ;
+
+    // int divided by int = int 
+    // to get float   float by int or double by int
+// or vice versa
+
+    cout <<( 3<5 )<< endl ; // return 1
+    // true = 1 false = 0
+ }
