605ebe9732bd648f54229a6f993a603d52d10752
Pratham <pratham10600@gmail.com>
2025-04-22 18:18:24 +0530

    sum_didgit

---
+++ lec5/sum_digits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace  std;
+int main(){
+    int a,sum=0;
+    cout<<"Enter number";
+    cin>>a;
+
+    while (a>0){
+        int last=a%10;
+        sum+=last;
+        a=a/10;
+        
+    }
+    cout<<sum;
+}
